eda6769164a701b50c12d509661cd5c7c64c31a8
ben-cunningham <bencunningham17@gmail.com>
2017-03-04 15:10:13 -0800

    secondary path

--- app/main.py
+++ app/main.py
@@ -8,6 +8,12 @@ taunts = [
     'i want callum',
 ]
 
+class food():
+    def __init__(self, loc, sld):
+        self.loc = loc
+        self.sld = sld
+
+
 @bottle.route('/static/<path:path>')
 def static(path):
     return bottle.static_file(path, root='static/')
@@ -91,22 +97,30 @@ def move():
     #             direction = 'right'
     #             print("Going RIGHT2")
 
-    closest_food_index = find_closest_food_index(snake_head, data)
-    first_food = food_list[closest_food_index]
+    sorted_list = get_food_list(snake_head, data)
+    first_food = sorted_list[0].loc
 
     primary_path = search(snake_head, data, first_food)
-
+    
+    has_sec_path = True
+    if len(sorted_list) > 1:
+        sec_food = sorted_list[1]
+        sec_path = search(first_food, data, sec_food)
+        has_sec_path = len(sec_path) > 0
+    
+    if not has_sec_path:
+        primary_path = search(snake_head, data, get_inverse_coord(board_width, board_height, data, snake_head))
 
     print ("Path -> " + primary_path.__str__())
 
     first_move = primary_path[-1]
-    if (up(snake_head) == first_move):
+    if (up(snake_head, 1) == first_move):
         direction = 'up'
-    if (down(snake_head) == first_move):
+    if (down(snake_head, 1) == first_move):
         direction = 'down'
-    if (left(snake_head) == first_move):
+    if (left(snake_head, 1) == first_move):
         direction = 'left'
-    if (right(snake_head) == first_move):
+    if (right(snake_head, 1) == first_move):
         direction = 'right'
 
     return {
@@ -114,12 +128,27 @@ def move():
         'taunt': random.choice(taunts),
     }
 
+def get_inverse_coord(board_width, board_height, data, snake_head):
+    x = board_width - snake_head[0]
+    y = board_height - snake_head[1]
 
-def find_closest_food_index(snake_head, data):
+    if is_safe([x, y], data):
+        return [x, y]
+
+    pos = [x,y]
+    rad = 1
+    while True:
+        return up(pos, rad) if is_safe(up(pos, rad), data)
+        return down(pos, rad) if is_safe(down(pos, rad), data)
+        return left(pos, rad) if is_safe(left(pos, rad), data)
+        return right(pos, rad) if is_safe(right(pos, rad), data)
+        rad += 1
+
+def get_food_list(snake_head, data):
     food_list = data.get('food')
     current_mindex = 0
     current_min_score = 999999
-    count = 0
+    l = []
     for current_food in food_list:
         current_distance = [99, 99]
         current_distance[0] = snake_head[0] - current_food[0]
@@ -127,13 +156,10 @@ def find_closest_food_index(snake_head, data):
         current_distance[0] = current_distance[0] * current_distance[0]
         current_distance[1] = current_distance[1] * current_distance[1]
         current_score = current_distance[0] + current_distance[1]
-        if current_score < current_min_score:
-            current_min_score = current_score
-            current_mindex = count
-        count += 1
-    return current_mindex
+        l.append(food(current_food, current_score) 
 
-
+    l.sort(key=lambda x: x.current_score)
+    return l
 
 def if_safe(new_snake_head, data):
     snakes = data.get('snakes')
@@ -157,20 +183,20 @@ def if_safe(new_snake_head, data):
     return True
 
 
-def up(old_snake_head):
-    return [old_snake_head[0], old_snake_head[1] - 1]
+def up(old_snake_head, rad):
+    return [old_snake_head[0], old_snake_head[1] - rad]
 
 
-def down(old_snake_head):
-    return [old_snake_head[0], old_snake_head[1] + 1]
+def down(old_snake_head, rad):
+    return [old_snake_head[0], old_snake_head[1] + rad]
 
 
-def left(old_snake_head):
-    return [old_snake_head[0] - 1, old_snake_head[1]]
+def left(old_snake_head, rad):
+    return [old_snake_head[0] - rad, old_snake_head[1]]
 
 
-def right(old_snake_head):
-    return [old_snake_head[0] + 1, old_snake_head[1]]
+def right(old_snake_head, rad):
+    return [old_snake_head[0] + rad, old_snake_head[1]]
 
 
 # Expose WSGI app (so gunicorn can find it)
